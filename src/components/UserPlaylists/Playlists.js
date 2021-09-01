a21ad4cd5c0e807e2928fd079284f8b4ecf9462f
Loh Jian Rong <ipwnjianrong@gmail.com>
2021-09-01 22:09:32 +0800

    added title to playlists page

--- src/components/UserPlaylists/Playlists.js
+++ src/components/UserPlaylists/Playlists.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Playlist from './Playlist';
+import sort from '../assets/sort.png';
 
 export default function Playlists({ userPlaylists }) {
   return (
     <div>
+      <p className="bg-gray-200 py-1 px-3 flex flex-row justify-between items-center">
+        <span className="font-semibold">YOUR PLAYLISTS</span>
+        <img 
+          src={sort} alt="sort" 
+          className="h-5 w-6"
+        />
+      </p>
       {userPlaylists.map(playlist => {
         return <Playlist key={playlist.id} playlist={playlist} />
       })}
