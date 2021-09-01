8102b3ebaa21783b0e551938e52391c801f408e7
Loh Jian Rong <ipwnjianrong@gmail.com>
2021-09-01 21:00:56 +0800

    Updated login page and added album covers

--- src/components/Main/Login.js
+++ src/components/Main/Login.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import traceify from '../assets/traceify-icon.png';
 
 const scope = 'user-read-private%20user-read-email%20user-top-read%20user-read-currently-playing%20user-read-playback-state%20user-modify-playback-state';
-// const redirect_uri = 'https://pedantic-wozniak-7be006.netlify.app/'
-const redirect_uri = 'http://localhost:3000'
+//https://traceify-client.netlify.app
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9eddabfa285a48e9800a63a292691f70&response_type=code&redirect_uri=https://traceify-client.netlify.app&scope=${scope}`
-// const AUTH_URL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_CLIENTID}&scope=${scope}&redirect_uri=http://localhost%3A3000`
-  // `?response_type=code` + 
-  // `&client_id=${process.env.REACT_APP_CLIENTID}` +
-  // `&scope=${scope}` +
-  // `&redirect_uri=${redirect_uri}`
 
 export default function Login() {
   return (
-    <a href={AUTH_URL} className="bg-green-50 w-5/12 items-center flex flex-col text-center rounded p-2">
-      <span className="text-green-800 font-semibold text-sm">Login with Spotify</span>
-    </a>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <img src={traceify} alt="logo" className="w-20" />
+      <a href={AUTH_URL} className="bg-green-100 text-center rounded-xl p-2">
+        <span className="text-green-800 font-semibold text-sm">Login with Spotify</span>
+      </a>
+    </div>
   )
 }
--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -11,13 +11,12 @@ export default function useAuth(code) {
     axios.post('https://traceify-server.herokuapp.com/login', {
       code
     }).then(res => {
-      console.log(res)
       setAccessToken(res.data.accessToken)
       setRefreshToken(res.data.refreshToken)
       setExpiresIn(res.data.expiresIn)
       // window.history.pushState({}, null, '/')
     }).catch(() => {
-      // window.location = '/'
+      window.location = '/'
     })
   }, [code])
 
