ce5b28dd1f9e420891aeb2532d37df29a1865d89
Loh Jian Rong <ipwnjianrong@gmail.com>
2021-09-01 12:11:00 +0800

    Updated url

--- src/components/Main/Login.js
+++ src/components/Main/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const scope = 'user-read-private user-read-email user-top-read user-read-currently-playing user-read-playback-state user-modify-playback-state';
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9eddabfa285a48e9800a63a292691f70&response_type=code&redirect_uri=http://localhost:3000&scope=${scope}`
+const redirect_uri = 'https://pedantic-wozniak-7be006.netlify.app/'
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9eddabfa285a48e9800a63a292691f70&response_type=code&redirect_uri=${redirect_uri}&scope=${scope}`
 
 export default function Login() {
   return (
